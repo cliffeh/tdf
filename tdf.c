ab297ce83b77c31d306c679a93e6a3ca6ff61d06
Cliff Snyder <cliff.snyder@gmail.com>
2021-12-23 20:29:22 -0500

    basic loop, clang formatting

--- tdf.c
+++ tdf.c
@@ -1,6 +1,16 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
+  FILE *fp;
+  int lineno = 0;
+  for (int i = 1; i < argc; i++)
+    {
+      if (!(fp = fopen (argv[i], "r")))
+        {
+          fprintf (stderr, "error: couldn't open file: %s\n", argv[i]);
+        }
+    }
 }
